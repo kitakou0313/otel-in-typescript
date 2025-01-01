eee4d431c9cc31116fd2d1c26fc7eabfc4941042
kitakou0313 <kitakou0313@gmail.com>
2025-01-01 21:57:45 +0900

    memo: add memo about implementation of Context

--- context-test.ts
+++ context-test.ts
@@ -6,6 +6,9 @@ import { NodeSDK } from "@opentelemetry/sdk-node";
 const ctx1 = api.context.getActive()
 const ctx2 = ctx1.setValue()
 
+// api.context.withで引数に与えたcontextをactiveなcontextとして実行できる
+// context.withの実装はContextManagerのwithを呼び出している
+// ContextManagerのwithメソッド内部ではAsyncLocalStorage.runをcontextを引数にして呼びだしている
 api.context.with(ctx1, () => {
     console.log(ctx2.getValue())
 })
